42d97291685d21d7f431ef52406df4cd138c5cba
Bodhisattva <48763354+Zinelabidiin@users.noreply.github.com>
2025-11-05 19:55:41 +0100

    Add files via upload

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+# Prompt the user for their current age
+current_age = int(input("How old are you? "))
+
+# Calculate age in 2050
+age_in_2050 = current_age + 25  # 2050 - 2025 = 25
+
+# Print the result
+print("In 2050, you will be", age_in_2050, "years old.")
